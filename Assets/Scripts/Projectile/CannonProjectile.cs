d47fb8607a0bee381ddd03a928e6790bcc6961ab
jimangseng <thehc9@gmail.com>
2024-10-14 18:21:11 +0900

    CannonProjectile 클래스 작성 중. Start() 실행 순서 문제의 근본적인 해결책은 무엇인가?

--- Assets/Scripts/Projectile/CannonProjectile.cs
+++ Assets/Scripts/Projectile/CannonProjectile.cs
@@ -6,14 +6,22 @@ using UnityEngine.UIElements;
 
 public class CannonProjectile : ProjectileBase
 {
+    // 대포알의 궤적 계산을 위한 Trace 객체
+    Trace trace;
+
     protected override void Start()
     {
         base.Start();
+
+        trace = new Trace();
     }
 
     public override void fire(Vector3 _from, Vector3 _to)
     {
         base.fire(_from, _to);
+
+        // Trace 객체의 Velocity값을 투사체 인스턴스의 velocity값으로 설정
+        projInstance.GetComponent<Rigidbody>().velocity = trace.Velocity;   // trace가 null이다. 또 Start() 전에 fire()가 실행되어 생기는 문제인데, 어떻게 해결할 수 있을까?
     }
 
 }
